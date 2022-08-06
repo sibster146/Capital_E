23e2bb97ebe3f44e8547699f4a6c7c9a9714c0f3
sibster146 <110351118+sibster146@users.noreply.github.com>
2022-08-05 21:24:50 -0500

    Add files via upload

---
+++ capitale/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+// import Header from "./components/Header";
+import React from "react";
+import NavigationBar from "./Components/NavigationBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
